b5a6a66eeb30e1321a694cc3451fa3f8d50976d9
jihuder <julioarenas85@gmail.com>
2023-03-28 20:12:16 -0500

    feat:funcion devuelve la lista de estudiantes filtrada por ciudad

---
+++ ES6_data_manipulation/4-update_grade_by_city.js
@@ -0,0 +1,16 @@
+export default function updateStudentGradeByCity(list, city, newGrades) {
+  return list
+    .filter((student) => student.location === city)
+    .map((student) => {
+      const std = student;
+      const grades = newGrades.filter(
+        (grade) => grade.studentId === student.id,
+      );
+      if (grades.length > 0) {
+        std.grade = grades[0].grade;
+      } else {
+        std.grade = 'N/A';
+      }
+      return student;
+    });
+}
